463a610883770dfacce854021ba69fab513d26c6
Yuverly Hidokun <133397996+YuverlyHidokun@users.noreply.github.com>
2024-11-22 20:59:35 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'blank',
+  appName: 'RoboPerros',
   webDir: 'www'
 };
 
